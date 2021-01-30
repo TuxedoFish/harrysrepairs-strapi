3b6f6234de6ce64cf104fbb51ae6f51b8685d4f1
TuxedoFish <harryliversedge@gmail.com>
2021-01-30 20:32:14 +0000

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ef6b4e8ffe437986a81b45496dcd8f5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9e85662f-3854-45cf-8243-c747d2f6d09f'
+};
