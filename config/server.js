e262ba9df7daf60ab40ce9f65dcab1fb8366f975
TuxedoFish <harryliversedge@gmail.com>
2021-01-31 15:06:54 +0000

    Added devices and pricings content

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 2000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '3ef6b4e8ffe437986a81b45496dcd8f5'),
